8e09657ea714fb17cbc7bcf06d5fcefdf6dfdcfe
Ubuntu <ubuntu@ip-172-31-6-229.us-west-2.compute.internal>
2020-06-15 02:03:49 +0000

    Smart enough to run in aws without my laptop's support.

--- scripts/options.pl
+++ scripts/options.pl
@@ -20,23 +20,23 @@ sub option {
     return $option{$_[0]};
 }
 
-# $my = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." ".&option("dbName");
-# $myV = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." -vvv ".&option("dbName");
-# $myVF = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." -vvv --force ".&option("dbName");
-# $myQ = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." --skip-column-names ".&option("dbName");
-# $myQF = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." --skip-column-names --force ".&option("dbName");
-# $myNo = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd");
+$my = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." ".option("dbName");
+$myV = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." -vvv ".option("dbName");
+$myVF = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." -vvv --force ".option("dbName");
+$myQ = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." --skip-column-names ".option("dbName");
+$myQF = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd")." --skip-column-names --force ".option("dbName");
+$myNo = option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd");
 
-$my = option("mysql")." ".&option("dbName");
-$myV = option("mysql")." -vvv ".&option("dbName");
-$myVF = option("mysql")." -vvv --force ".&option("dbName");
-$myQ = option("mysql")." --skip-column-names ".&option("dbName");
-$myQF = option("mysql")." --skip-column-names --force ".&option("dbName");
-$myNo = option("mysql");
+#$my = option("mysql")." ".&option("dbName");
+#$myV = option("mysql")." -vvv ".&option("dbName");
+#$myVF = option("mysql")." -vvv --force ".&option("dbName");
+#$myQ = option("mysql")." --skip-column-names ".&option("dbName");
+#$myQF = option("mysql")." --skip-column-names --force ".&option("dbName");
+#$myNo = option("mysql");
 
 sub db_exists {
     $dbName = $_[0];
-    $cmd = "echo \"show databases;\" | ".&option("mysql");
+    $cmd = "echo \"show databases;\" | ".&option("mysql")." -u ".option("dbUser")." --password=".option("dbPwd");
     @dbs = `$cmd`;
     chomp(@dbs);
     foreach (@dbs) {
